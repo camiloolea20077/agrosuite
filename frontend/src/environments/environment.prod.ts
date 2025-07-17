1ece42259f1985cad0bc7f535f5837a2d54d9b46
camiloolea20077 <112830645+camiloolea20077@users.noreply.github.com>
2025-07-17 10:33:54 -0500

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: true,
-    apiUrl: 'agrosuite-production.up.railway.app/',
-    birthsUrl: 'agrosuite-production.up.railway.app/births',
-    usersUrl: 'agrosuite-production.up.railway.app/users',
-    authUrl: 'agrosuite-production.up.railway.app/auth',
-    cattleUrl: 'agrosuite-production.up.railway.app/cattle',
-    employeesUrl: 'agrosuite-production.up.railway.app/employees',
-    listElementUrl: 'agrosuite-production.up.railway.app/list-elements',
-    dashboardUrl: 'agrosuite-production.up.railway.app/dashboard'
+    apiUrl: 'https://agrosuite-production.up.railway.app/',
+    birthsUrl: 'https://agrosuite-production.up.railway.app/births',
+    usersUrl: 'https://agrosuite-production.up.railway.app/users',
+    authUrl: 'https://agrosuite-production.up.railway.app/auth',
+    cattleUrl: 'https://agrosuite-production.up.railway.app/cattle',
+    employeesUrl: 'https://agrosuite-production.up.railway.app/employees',
+    listElementUrl: 'https://agrosuite-production.up.railway.app/list-elements',
+    dashboardUrl: 'https://agrosuite-production.up.railway.app/dashboard'
 };
